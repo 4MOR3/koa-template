086f9fffb781171db9e173683567f5a33b9079e2
Vaapad <39641565+4MOR3@users.noreply.github.com>
2022-03-24 14:43:54 +0800

    init

---
+++ README.md
@@ -0,0 +1,14 @@
+# koa-demo
+[![JavaScript Style Guide](https://cdn.rawgit.com/standard/standard/master/badge.svg)](https://github.com/standard/standard)
+[![JavaScript Style Guide](https://img.shields.io/badge/code_style-standard-brightgreen.svg)](https://standardjs.com)
+
+## Project setup
+# notice: puppetter npm package is pretty large (over 300M) so you have to be patient
+```
+npm install
+```
+
+### Compiles and hot-reloads for development
+```
+npm run start
+```
---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import Koa from 'koa'
+import Router from './router'
+import Api from './Api'
+
+declare module 'koa' { 
+  interface Context<T = Api.BaseApi> { 
+    body: T
+  }
+}
+async function handler(ctx: Koa.Context<Api.BaseApi>, next: Koa.Next): Promise<void>{
+  try {
+    await next();
+  } catch (err: any) {
+    console.log('err caught')
+    ctx.body = {
+      code: 2,
+      message: err.toString(),
+      data: {}
+    }
+  }
+};
+
+async function main(ctx: Koa.Context, next: Koa.Next): Promise<void> {
+  await next()
+}
+
+const app = new Koa()
+app.use(handler)
+app.use(main)
+app.use(Router.routes());
+app.use(Router.allowedMethods());
+
+app.listen(4396)
