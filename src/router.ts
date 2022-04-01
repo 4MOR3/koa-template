d5a8e9060e82f9589bc21cea415b597eecec7d0c
Vaapad <39641565+4MOR3@users.noreply.github.com>
2022-04-01 14:55:31 +0800

    Update router.ts
    add 注释

--- src/router.ts
+++ src/router.ts
@@ -1,6 +1,9 @@
 import Router from 'koa-router'
 
 const router = new Router()
+// import someFunc from './controller/someFunc'
+// router.get('/path',someFunc)
+// register your router like this
 router.get('', function () { })
 
 
